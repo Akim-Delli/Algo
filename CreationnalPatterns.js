2252291a254e5ccf2933b327afc465a891d6d7ff
Akim Delli <adelli@liveintent.com>
2015-01-05 23:19:00 -0500

    Object.create()

--- CreationnalPatterns.js
+++ CreationnalPatterns.js
@@ -2,31 +2,63 @@
  * Created by adelli on 1/4/15.
  * JS Creational patterns
  */
+(function () {
+    'use strict';
 
-var obj = {
-    occupation : "software Engineer",
-    lastName : "Dell"
-};
+    var obj = {
+        occupation: "software Engineer",
+        lastName: "Dell"
+    };
 
-obj.firstName = "Akim";
-obj["lastName"] = "Delli";
+    obj.firstName = "Akim";
+    obj["lastName"] = "Delli";
 
 
-var firstName = obj["firstName"];
-var lastName =  obj.lastName;
+    var firstName = obj["firstName"];
+    var lastName = obj.lastName;
 
 
 // EMACScript 5
-Object.defineProperty(obj, 'country', {
-    value : 'USA'
-});
+    Object.defineProperty(obj, 'country', {
+        value: 'USA'
+    });
 
-Object.defineProperties(obj, {
-    twitter : {
-        value : 'adelli'
-    },
-    email : {
-        value : 'akim_delli@hotmail.com'
-    }
-});
+    Object.defineProperties(obj, {
+        twitter: {
+            value: 'adelli'
+        },
+        email: {
+            value: 'akim_delli@hotmail.com'
+        }
+    });
 
+// Object Creation with Object.Create() ECMACScript 5
+
+    var JohnDoe, JaneDoe, sayName, greeting;
+    JohnDoe = {
+        firstName: "John",
+        lastName: "Doe",
+        sayName: function () {
+            return "Hello I'm " + this.firstName;
+        }
+    };
+    JaneDoe = Object.create(JohnDoe, {
+        firstName: {
+            value: "Jane"
+        },
+        lastName: {
+            value: "Doe"
+        },
+        greet: {
+            value: function (Person) {
+                return "Hello " + Person.firstName;
+            }
+        }
+    });
+    sayName = JaneDoe.sayName();
+    greeting = JaneDoe.greet(JohnDoe);
+
+    console.log(sayName);
+    console.log(greeting);
+
+}());
