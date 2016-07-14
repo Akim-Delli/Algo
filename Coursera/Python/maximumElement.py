a31d67bdf8846ef64ed6d06f7da209cd3454c091
Akim Delli <adelli@hearst.com>
2016-07-14 16:33:23 -0400

    renames function

--- Coursera/Python/maximumElement.py
+++ Coursera/Python/maximumElement.py
@@ -4,19 +4,19 @@
 import math
 
 
-def maximumElement(arr):
+def maximum_element(arr):
     if len(arr) == 1:
         return arr[0]
 
     midpoint = math.floor(len(arr)/2)
     print("midpoint", midpoint)
     if arr[midpoint-1] > arr[midpoint]:
-        return maximumElement(arr[:midpoint])
+        return maximum_element(arr[:midpoint])
     else:
-        return maximumElement(arr[midpoint:])
+        return maximum_element(arr[midpoint:])
 
 test_case1 = [1, 2, 3, 4, 5, 6, 3, 2, 0]
 test_case2 = [11, 20, 33, 14, 10, 9, 8, 7, 5, 3, 2, 0]
 
-print("Maximum number for ", test_case1, maximumElement(test_case1))
-print("Maximum number for ", test_case2, maximumElement(test_case2))
+print("Maximum number for ", test_case1, maximum_element(test_case1))
+print("Maximum number for ", test_case2, maximum_element(test_case2))
